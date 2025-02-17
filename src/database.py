cfa931d1ef58fc92a3c7b839d68da6ac8e73cb5d
Theskyguard4 <lewispeck200@gmail.com>
2025-02-17 21:25:34 +0000

    added basic booking management, needs token authentication and database interaction

--- src/database.py
+++ src/database.py
@@ -19,3 +19,11 @@ class DatabaseManager:
     """Database class the has functions mapped to identified SQL queries that are necersarry"""
     def __init__(self, file: str = "database.db", create_script: str = "src/create.sql"):
         pass
+    def find_booking(self, room: Room, time: int):
+        return None
+    def find_booking(self, id: int):
+        return None
+    def add_booking(self, room: Room, time: int, user: User):
+        return None
+    def remove_booking(self, id: int):
+        return None
