69b085b392caccbe97db294ee748ba5a9ea394a3
forester302 <109694860+forester302@users.noreply.github.com>
2025-02-24 15:30:50 +0000

    Add Return types to database functions

--- src/database.py
+++ src/database.py
@@ -21,37 +21,37 @@ class DatabaseManager:
     #sql queries should be put into these functions to interact with the database
     def __init__(self, file: str = "database.db", create_script: str = "src/create.sql"):
         pass
-    def verify_token(self, token: str):
+    def verify_token(self, token: str) -> bool:
         user = self.get_user(token) 
         if not user:
             return False
         return True
-    def find_booking(self, room: Room, time: int):
+    def find_booking(self, room: Room, time: int) -> Booking:
         # return booking object using room and time
-        return None
-    def find_booking(self, id: int):
+        return Booking(0, User(""), Room(0), 0)
+    def get_booking(self, id: int) -> Booking:
         # return booking object using id
-        return None
-    def add_booking(self, room: Room, time: int, token: str):
+        return Booking(0, User(""), Room(0), 0)
+    def add_booking(self, room: Room, time: int, token: str) -> None:
         # add booking to database
         user = self.get_user(token) 
         if not user:
             return None
         return None
-    def remove_booking(self, id: int):
+    def remove_booking(self, id: int) -> None:
         # remove booking from database
-        booking = self.find_booking(id)
+        booking = self.get_booking(id)
         return None
-    def get_all_bookings(self):
+    def get_all_bookings(self) -> list[Booking]:
         # gets a list of all future bookings (not past)
         return []
-    def get_room(self, room_id: int):
+    def get_room(self, room_id: int) -> Room:
         # get room object using room_id
-        return None
-    def get_user(self, token:str):
+        return Room(0)
+    def get_user(self, token:str) -> User:
         # get user object using token
-        return None
-    def get_user(self, booking_id: int):
+        return User("")
+    def get_user_from_booking(self, booking_id: int) -> User:
         # get user object using booking_id
-        return None
+        return User("")
     
